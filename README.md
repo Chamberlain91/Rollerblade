a0fc5ce071921458b339f704323da5fa67374ea6
Christopher Chamberlain <chamberlain@live.ca>
2020-05-19 21:07:59 -0600

    Added ability to compile Sass to Css

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 # Rollerblade
 
 ## Overview
-A tool to shortcut compiling typescript to a single bundle.
+A command line tool and npm module to shortcut compiling typescript and sass into individual bundles.
 
 ## Installation
 
@@ -17,6 +17,8 @@ To install as a build dependancy
 npm install rollerblade tslib
 ```
 
+**Note:** Including `tslib` here seemed to be necessary for some reason, but I would like for all dependencies behind rollerblade.
+
 ## CLI Usage
 
 ```bash
@@ -25,20 +27,21 @@ rollerblade input [output] --tsconfig mytsconfig.json
 
 ### CLI Configuration
 
-* `-c` or `--tsconfig` to use custom tsconfig
+Parameter `input` must be a `.ts`, `.scss` or `.sass` file and `output` will be either `.js` or `.css`. Omission of `output` will result in the compiled stylesheet or javascript placed next to the input file. Source maps are automatically placed next to the output file with `.map` appended to the output file name.
 
-If the output parameter is not specified, the compiled javascript is placed next to the input file.
+* `-c` or `--tsconfig` to use custom tsconfig during typescript compilation
+
+Multiple invocations of the CLI are needed in order to compile multiple bundles or you must use the Javascript API.
 
 ## Javascript API
 
 ```js
 import rollerblade from 'rollerblade'
-for (let result of await rollerblade({ input, output, tsconfig })) {
-    fs.writeFileSync(result.file, result.text);
-}
 ```
 
-The `config` object passed into rollerblade is formatted like:
+The `rollerblade` function accepts one or more `config` objects and returns a promise for one or more `output` objects.
+
+The `config` objects are formatted like:
 
 ```ts
 {
@@ -48,4 +51,34 @@ The `config` object passed into rollerblade is formatted like:
 }
 ```
 
-You may specifiy one or more config objects to compile multiple bundles via the js API.
+The `output` objects are formatted like:
+
+```ts
+{
+    file: string,
+    text: string
+}
+```
+
+### Example
+
+For example, to compile both a typescript file and a stylesheet.
+
+```js
+const rollerblade = require("rollerblade");
+const fs = require("fs");
+
+(async () => {
+
+    for (let { file, text } of await rollerblade([
+        { input: "src/script/index.ts" },
+        { input: "src/style/index.scss" }
+    ])) {
+        // Write each generated file to disk
+        // In this example 4 files. Two compiled bundles
+        // and two source maps.
+        fs.writeFileSync(file, text);
+    }
+
+})()
+```
--- src/index.ts
+++ src/index.ts
@@ -6,6 +6,8 @@ import { terser } from 'rollup-plugin-terser'
 import rollup from 'rollup'
 import chalk from 'chalk';
 
+import sass from "sass"
+
 import * as p from 'path';
 
 function changeExtension(src: string, ext: string) {
@@ -39,105 +41,158 @@ export default async function rollerblade(configs: Config[] | Config) {
         // Resolve full path
         config.input = p.resolve(config.input);
 
-        // Default output to just renaming .ts to .js
-        if (config.output == undefined)
-            config.output = changeExtension(config.input, "js");
+        // Compile typescript
+        if (config.input.endsWith(".ts")) {
 
-        // Log progress
-        console.log(chalk.cyanBright(`Processing: '${config.input}' -> '${config.output}'`));
-
-        try {
-
-            const bundle = await rollup.rollup({
-                // Configure files
-                input: config.input,
-                output: {
-                    file: config.output,
-                    sourcemap: true
-                },
-                // Configure rollup
-                treeshake: true,
-                // Configure plugins
-                plugins: [
-                    // Compile TS
-                    typescript({
-                        // importHelpers: true,
-                        removeComments: true,
-                        // moduleResolution: 'node',
-                        tsconfig: config.tsconfig ?? false,
-                        allowSyntheticDefaultImports: true,
-                        experimentalDecorators: true,
-                        emitDecoratorMetadata: true,
-                        inlineSources: true,
-                        sourceMap: true,
-                        strict: true,
-                        target: "ES2018",
-                        lib: [
-                            "ES2015",
-                            "ES2016",
-                            "ES2017",
-                            "ES2018",
-                            "ESNext",
-                            "DOM",
-                            "DOM.Iterable"
-                        ]
-                    }),
-                    // Compile JS
-                    resolve(),
-                    commonjs(),
-                    terser({
-                        compress: {
-                            ecma: 2018,
-                            unsafe: true,
-                            unsafe_math: true,
-                            unsafe_arrows: true,
-                            unsafe_undefined: true,
-                            unsafe_proto: true,
-                            unsafe_methods: true,
-                        }
-                    }),
-                    // Emit Source Maps
-                    sourceMaps(),
-                ]
-            });
-
-            // Generate output
-            const { output } = await bundle.generate({
-                sourcemap: "inline",
-                format: "cjs"
-            });
-
-            // For each output chunk or asset
-            for (const result of output) {
-
-                if (result.type == "asset") {
-                    // Is asset
-                    console.warn(`Unable to handle asset '${result.fileName}'`);
-                } else {
-
-                    // Get source map file name
-                    const mapFile = config.output + '.map';
-                    const relativeMapFile = p.relative(p.dirname(config.output), mapFile);
-
-                    // Is chunk
-                    results.push({
-                        file: config.output,
-                        text: result.code + `//# sourceMappingURL=${relativeMapFile}`
-                    });
-
-                    // Is chunk
-                    results.push({
-                        file: mapFile,
-                        text: JSON.stringify(result.map),
-                    });
-                }
+            for await (let result of compileTypescript(config)) {
+                results.push(result);
             }
+        }
+        // Compile sass/scss
+        else if (config.input.endsWith(".scss") || config.input.endsWith(".sass")) {
 
-        } catch (e) {
-            // 
-            console.error(e);
+            for await (let result of compileSass(config)) {
+                results.push(result);
+            }
         }
     }
 
     return results;
+}
+
+async function* compileTypescript(config: Config) {
+
+    // Default output to just renaming .ts to .js
+    if (config.output == undefined)
+        config.output = changeExtension(config.input, "js");
+
+    // Log progress
+    console.log(chalk.cyanBright(`Processing: '${config.input}' -> '${config.output}'`));
+
+    try {
+
+        const bundle = await rollup.rollup({
+            // Configure files
+            input: config.input,
+            output: {
+                file: config.output,
+                sourcemap: true
+            },
+            // Configure rollup
+            treeshake: true,
+            // Configure plugins
+            plugins: [
+                // Compile TS
+                typescript({
+                    // importHelpers: true,
+                    removeComments: true,
+                    // moduleResolution: 'node',
+                    tsconfig: config.tsconfig ?? false,
+                    allowSyntheticDefaultImports: true,
+                    experimentalDecorators: true,
+                    emitDecoratorMetadata: true,
+                    inlineSources: true,
+                    sourceMap: true,
+                    strict: true,
+                    target: "ES2018",
+                    lib: [
+                        "ES2015",
+                        "ES2016",
+                        "ES2017",
+                        "ES2018",
+                        "ESNext",
+                        "DOM",
+                        "DOM.Iterable"
+                    ]
+                }),
+                // Compile JS
+                resolve(),
+                commonjs(),
+                terser({
+                    compress: {
+                        ecma: 2018,
+                        unsafe: true,
+                        unsafe_math: true,
+                        unsafe_arrows: true,
+                        unsafe_undefined: true,
+                        unsafe_proto: true,
+                        unsafe_methods: true,
+                    }
+                }),
+                // Emit Source Maps
+                sourceMaps(),
+            ]
+        });
+
+        // Generate output
+        const { output } = await bundle.generate({
+            sourcemap: "inline",
+            format: "cjs"
+        });
+
+        // For each output chunk or asset
+        for (const result of output) {
+
+            if (result.type == "asset") {
+                // Is asset
+                console.warn(`Unable to handle asset '${result.fileName}'`);
+            } else {
+
+                // Get source map file name
+                const mapFile = config.output + '.map';
+                const relativeMapFile = p.relative(p.dirname(config.output), mapFile);
+
+                // Is chunk
+                yield <Output>{
+                    file: config.output,
+                    text: result.code + `//# sourceMappingURL=${relativeMapFile}`
+                };
+
+                // Is chunk
+                yield <Output>{
+                    file: mapFile,
+                    text: JSON.stringify(result.map),
+                };
+            }
+        }
+
+    } catch (e) {
+        // 
+        console.error(e);
+    }
+}
+
+
+async function* compileSass(config: Config) {
+
+    // Default output to just renaming .ts to .js
+    if (config.output == undefined)
+        config.output = changeExtension(config.input, "css")
+
+    // Log progress
+    console.log(chalk.cyanBright(`Processing: '${config.input}' -> '${config.output}'`))
+
+    let result = sass.renderSync({
+        file: config.input,
+        outFile: config.output,
+        outputStyle: "compressed",
+        sourceMapContents: true,
+        sourceMap: true
+    })
+
+    // Emit generated css
+    yield <Output>{
+        file: config.output,
+        text: result.css.toString()
+    }
+
+    // If a sourcemap has been generated
+    if (result.map) {
+
+        // Emit source map
+        yield <Output>{
+            file: config.output + ".map",
+            text: result.map.toString()
+        }
+    }
 }
